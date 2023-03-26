48985eee716c3e354b174f63ea5f470cfc6047e6
codeforpasssion <codeforpasssion@gmail.com>
2023-03-26 21:47:08 +0530

    add loading effect

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type Props = {};
+
+const Loading = (props: Props) => {
+  return (
+    <div className="flex items-center text-center justify-center align-middle">
+      <h1>Loading...</h1>
+    </div>
+  );
+};
+
+export default Loading;
